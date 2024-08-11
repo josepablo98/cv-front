79756f172dc95693f849fcbb6f4e042e33becd31
Jose Pablo <josepabloruizperez8@gmail.com>
2024-08-11 12:22:01 +0200

    changing to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppRouter } from './routes/AppRouter'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename='/cv-front'>
+    <HashRouter>
       <AppRouter />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
