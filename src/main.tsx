7bc3878f0cf8245791d2a61edc0a9f1db7dff6bd
Jose Pablo <josepabloruizperez8@gmail.com>
2024-08-11 12:23:46 +0200

    docs updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './routes/AppRouter'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/cv-front'>
       <AppRouter />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
