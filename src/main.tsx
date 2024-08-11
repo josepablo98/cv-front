6093612bfd349dfe951aa355f3e30c376ed47e17
Jose Pablo <josepabloruizperez8@gmail.com>
2024-08-11 12:10:35 +0200

    paths fixed

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { AppRouter } from './routes/AppRouter'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/cv-front'>
       <AppRouter />
     </BrowserRouter>
   </React.StrictMode>,
